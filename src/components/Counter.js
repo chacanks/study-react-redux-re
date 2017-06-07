0107056605c7489674be69d76de7d7faba69010b
이 강선 <chacanks@K.local>
2017-06-07 23:30:17 +0900

    redux re finish

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,6 +2,10 @@ import React, {Component, PropTypes} from 'react';
 import Value from './Value';
 import Control from './Control';
 
+import * as actions from '../actions';
+
+import {connect} from 'react-redux';
+
 const propTypes = {
 
 };
@@ -14,28 +18,34 @@ class Counter extends React.Component{
 
     constructor(props){
         super(props);
+        this.setRandomColor = this.setRandomColor.bind(this);
     }
 
-    handlePlus(){
-        console.log('handlePlus')
-    }
 
-    handleMinus(){
-        console.log('handleMinus')
-    }
+    setRandomColor(){
+        const color = [
+            Math.floor((Math.random() * 55) + 200)
+            , Math.floor((Math.random() * 55) + 200)
+            , Math.floor((Math.random() * 55) + 200)
+        ];
 
-    handleSetColor(){
-        console.log('handleSetColor')
+        this.props.handleSetColor(color);
     }
 
     render(){
+
+        const color = this.props.color;
+        const style = {
+            background : `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        }
+
         return(
-            <div>
-                <Value />
+            <div style={style}>
+                <Value number={this.props.number}/>
                 <Control 
-                    onPlus={this.handlePlus}
-                    onMinus={this.handleMinus}
-                    onSetColor={this.handleSetColor}
+                    onPlus={this.props.handleIncrement}
+                    onMinus={this.props.handleDecrement}
+                    onSetColor={this.setRandomColor}
                 />
             </div>
         );
@@ -45,4 +55,19 @@ class Counter extends React.Component{
 Counter.propTypes = propTypes;
 Counter.defaultProps = defaultProps;
 
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        number : state.counter.number
+        , color : state.ui.color
+    };
+}
+
+const mapDispathToProps = (dispatch) =>{
+    return {
+        handleIncrement : () => {dispatch(actions.increment())}
+        , handleDecrement : () => {dispatch(actions.decrement())}
+        , handleSetColor : (color) => {dispatch(actions.setColor(color))}
+    }
+};
+
+export default connect(mapStateToProps, mapDispathToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+import {createStore} from 'redux';
+import reducers from './reducers';
 
+import {Provider} from 'react-redux'
+
+//store 생성
+const store = createStore(reducers);
 
 ReactDOM.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
     , document.getElementById('root')
 );
+
